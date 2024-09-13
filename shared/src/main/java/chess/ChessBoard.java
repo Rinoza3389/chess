23dafb7476217d2647006923f5ab7d788972c91f
Rinoza3389 <maddy.lorrigan@gmail.com>
2024-09-13 16:54:39 -0600

    Setting up ChessBoard to work... eventually

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -171,4 +174,16 @@ public class ChessBoard {
         addPiece(BP8_position, BP8_piece);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
+    }
 }
